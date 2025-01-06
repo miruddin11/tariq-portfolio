25c79589c0e9bb1e85064e654326ce42076eb349
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2025-01-07 01:06:24 +0530

    Update Header.jsx
    Putting the correct file.

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -53,7 +53,7 @@ const header = () => {
         initial={{y:30,opacity: 0}}
         whileInView={{y:0,opacity:1}}
         transition={{duration:0.6,delay:1.2}}
-        href="/sample-resume.pdf" download 
+        href="/Mir-Tariquddin.pdf" download 
         className='px-10 py-3 border rounded-full borderd-gray-500 flex
         items-center gap-2 bg-white dark:text-black'>my resume<Image src={assets.download_icon} alt='' 
         className='w-4'/></motion.a>
