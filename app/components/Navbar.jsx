c6fb924ed10e1bc9361d7bb2f05282427024f860
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2025-01-28 19:46:43 +0530

    "commit"

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = ({isDarkMode, setIsDarkMode}) => {
             <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%] dark:hidden'>
                 <Image src={assets.header_bg_color} alt="" className='w-full'/>
             </div>
-            <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm dark:shadow-white/20 " : ""}`}>
+            <nav className={`w-full fixed top-0 left-0 px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm dark:shadow-white/20 " : ""}`}>
                 <a href="#top">
                     <Image 
                         src={isDarkMode ? assets.logo_dark : assets.logo} 
